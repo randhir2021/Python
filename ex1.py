21b9b9002d28905a94b2bdb676b44904b8c459a6
randhir2021 <randhir2021@gmail.com>
2016-10-15 19:58:26 +0530

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,10 @@
+count = 0
+while (count < 10):
+    print "The count is:", count
+    count = count + 1
+
+print "Good Bye!"
+
+
+for letter in 'Python':     # First Example
+   print 'Current Letter :', letter
